e92d07c56fcfacc2c81cbf1da6b1e4b984954583
Jouni Männistö <jouni.mannisto@gmail.com>
2018-01-31 18:14:13 +0200

    Julkaisu 1.0.0

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 const apiPrefix = '/api'
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(bodyParser.json())
